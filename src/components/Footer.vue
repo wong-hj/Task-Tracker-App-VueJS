e7aec0c47e39a95c80df550beabe0db857335530
WONG HORNG JUN <hj.wong02@gmail.com>
2022-05-31 17:45:19 +0800

    Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <p>Copyright &copy; 2022</p>
+        <router-link to="/about">About</router-link>
+    </footer>
+</template>
+
+<style scope>
+    a {
+        color: #333;
+    }
+
+    footer {
+        margin-top:30px;
+        text-align: center;
+    }
+</style>
